dd735d4148c9ebf4bed22fbe913038e3a6c22fcc
Fenorik <pedro.apatrocinio.aquino@gmail.com>
2025-06-08 18:16:17 -0300

    Fenorik Init

---
+++ FenorikHubINIT.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 109983668079237 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubSTAB.lua"))()
+else
+  warn("Feronik Hub does not support your currently game.")
+end

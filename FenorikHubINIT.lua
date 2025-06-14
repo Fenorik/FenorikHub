4ac101078abcb3838d0d695525a824c0cdcc7f95
Fenorik <pedro.apatrocinio.aquino@gmail.com>
2025-06-14 12:11:48 -0300

    Update FenorikHubINIT.lua

--- FenorikHubINIT.lua
+++ FenorikHubINIT.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 109983668079237 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubSTAB.lua"))()
+elseif game.PlaceId == 126884695634066 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubGAG.lua"))()
 else
   warn("Feronik Hub does not support your currently game.")
 end

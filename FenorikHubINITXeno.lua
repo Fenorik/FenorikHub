4a24d0c8e21e3b690b6c6c8e24ca1093e0b25d71
Fenorik <pedro.apatrocinio.aquino@gmail.com>
2025-06-25 20:48:34 -0300

    Delete FenorikHubINITXeno.lua

--- FenorikHubINITXeno.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 109983668079237 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubSTABXeno.lua"))()
-elseif game.PlaceId == 126884695634066 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubGAGXeno.lua"))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Fenorik/FenorikHub/refs/heads/main/FenorikHubUNIVERSAL.lua"))()
-end
